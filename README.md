7449d20b184077cc77231baeba9c6607df277435
vincent178 <vh7157@gmail.com>
2024-02-13 21:19:16 +0800

    update

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 Machine Learning Notes
 ===
 
+机器学习
+
+* 分类问题
+* 回归问题
+https://zhuanlan.zhihu.com/p/94152890 (未读)
+
+经典统计学习技术中的线性回归和softmax回归可以视为线性神经网络
+https://zh.d2l.ai/chapter_linear-networks/index.html
+
+算法
+
+* LMS 算法
+
+## 提问
+
+1. 什么是回归 
+2. 矩阵，张量和N维空间是什么关系，如何可视化表现
 
 
---
+++ linear_neural_network.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+# 经典统计学习技术中的线性回归和softmax回归可以视为线性神经网络
+# https://zh.d2l.ai/chapter_linear-networks/index.html
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def identity_function(x):
+    return x
+
+def init_network():
+    network = {}
+    network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    network['b1'] = np.array([0.1, 0.2, 0.3])
+    network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    network['b2'] = np.array([0.1, 0.2])
+    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
+    network['b3'] = np.array([0.1, 0.2])
+    return network
+
+def forward(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = identity_function(a3)
+
+    return y
+
+network = init_network()
+x = np.array([1.0, 0.5])
+y = forward(network, x)
+print(y)
